27178155aa48def91903b84f1b63dc6471bfaf5e
Philipp Knobel <philnate@gmail.com>
2017-09-24 17:10:43 +0200

    initial commit by Atomist

---
+++ src/main/java/com/github/cherimojava/orchidae/filems/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.cherimojava.orchidae.filems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+}
